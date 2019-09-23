29d54823881746192d858cf0e6740122b9c0f9d4
Eftekher.Aqib-bjit <eftekher.aqib@bjitgroup.com>
2019-09-23 17:06:46 +0600

    Initial commit

---
+++ app/src/main/java/com/eftekher/messmanager/Utils.java
@@ -0,0 +1,13 @@
+package com.eftekher.messmanager;
+
+public class Utils {
+	
+	//Email Validation pattern
+	public static final String regEx = "(+880/0)?[1][1-9]{10}";
+
+	//Fragments Tags
+	public static final String Login_Fragment = "Login_Fragment";
+	public static final String SignUp_Fragment = "SignUp_Fragment";
+	public static final String ForgotPassword_Fragment = "ForgotPassword_Fragment";
+	
+}
